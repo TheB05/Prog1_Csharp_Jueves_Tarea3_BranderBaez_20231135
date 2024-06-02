b6b02a75a5d9c57b7314336cf19df702e4a7ec8d
MSI ACCOUNT <MSI ACCOUNT@MSI>
2024-06-01 21:00:25 -0400

    Add project files.

---
+++ Prog1_Csharp_Jueves_Tarea3_20231135/Empleado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog1_Csharp_Jueves_Tarea3_20231135
+{
+    public class Empleado: MiembroDeLaComunidad
+    {
+        public int EmpleadoId { get; set; }
+    }
+}
